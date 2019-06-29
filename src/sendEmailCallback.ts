c56baac5e2bcf991ccb92467bd56ffadfd1feb09
Insik Cho <insik.cho@alaskaair.com>
2019-06-29 10:13:39 -0700

    removed unused required

--- src/sendEmailCallback.ts
+++ src/sendEmailCallback.ts
@@ -1,6 +1,5 @@
 export {};
 const fs = require('fs');
-const fetch = require('node-fetch');
 const sendEmail = require('./sendEmail');
 const secrets = require('./secrets');
 const addRecipients = require('./addRecipients');
